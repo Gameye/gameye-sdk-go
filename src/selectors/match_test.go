9bc293a56e95da55a8238e3e654fc8bfabb246de
Elmer Bulthuis <elmerbulthuis@gmail.com>
2018-08-21 15:24:39 +0200

    adds tests

---
+++ src/selectors/match_test.go
@@ -0,0 +1,26 @@
+package selectors
+
+import (
+	"testing"
+)
+
+func TestSelectMatchList(test *testing.T) {
+	// const matchList = selectMatchList(mocks.matchStateMock);
+	// t.equal(matchList.length, 2);
+	// t.equal(matchList.filter(i => i.matchKey === "test-match-123").length, 1);
+	// t.equal(matchList.filter(i => i.matchKey === "test-match-456").length, 1);
+}
+
+func TestSelectMatchListForGame(test *testing.T) {
+	// const matchList = selectMatchListForGame(mocks.matchStateMock, "test");
+	// t.equal(matchList.length, 1);
+	// t.equal(matchList.filter(i => i.matchKey === "test-match-123").length, 1);
+}
+
+func TestSelectMatchItem(test *testing.T) {
+	// const matchItem = selectMatchItem(mocks.matchStateMock, "test-match-123");
+	// t.ok(matchItem);
+	// if (matchItem) {
+	//     t.equal(matchItem.matchKey, "test-match-123");
+	// }
+}
