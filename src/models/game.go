111f4ff960700f80e10637d6a427c737577b69d3
Elmer Bulthuis <elmerbulthuis@gmail.com>
2018-08-21 14:30:07 +0200

    adds models

--- src/models/game.go
+++ src/models/game.go
@@ -1,1 +1,18 @@
 package models
+
+type GameQueryState struct {
+	Game     GameQueryGameIndex
+	Location GameQueryLocationIndex
+}
+
+type GameQueryGameIndex = map[string]*GameQueryGameItem
+
+type GameQueryLocationIndex = map[string]*GameQueryLocationItem
+
+type GameQueryGameItem struct {
+	GameKey  string
+	Location map[string]bool
+}
+type GameQueryLocationItem struct {
+	LocationKey string
+}
--- src/models/template.go
+++ src/models/template.go
@@ -1,1 +1,35 @@
 package models
+
+type TemplateQueryState struct {
+	Template TemplateQueryArgIndex
+}
+
+type TemplateQueryArgIndex = map[string]*TemplateQueryArgItem
+
+type TemplateQueryArgItem struct {
+	TemplateKey string
+	Arg         ArgConfig
+}
+
+type ArgConfig = []ArgConfigItem
+
+type ArgConfigItem = interface{}
+
+type NumberArgConfigItem struct {
+	// type "number"
+	Name             string
+	DefaultValue     int
+	MinimumValue     int
+	MinimumExclusive bool
+	MaximumValue     int
+	MaximumExclusive bool
+	Option           []int
+}
+type StringArgConfigItem struct {
+	// type "string"
+	Name               string
+	DefaultValue       string
+	ValidatePattern    string
+	ValidateIgnoreCase bool
+	Option             []string
+}
