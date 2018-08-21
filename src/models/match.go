167b00bdef06e4eacadb1bc3c1cb23d9f536b805
Elmer Bulthuis <elmerbulthuis@gmail.com>
2018-08-21 14:30:07 +0200

    adds models

--- src/models/match.go
+++ src/models/match.go
@@ -1,1 +1,16 @@
 package models
+
+type MatchQueryState struct {
+	Match MatchQueryMatchIndex
+}
+
+type MatchQueryMatchIndex = map[string]*MatchQueryMatchItem
+
+type MatchQueryMatchItem struct {
+	MatchKey    string
+	GameKey     string
+	LocationKey string
+	Host        string
+	Created     int
+	Port        map[string]int
+}
--- src/models/statistic.go
+++ src/models/statistic.go
@@ -1,1 +1,40 @@
 package models
+
+type StatisticQueryState struct {
+	statistic AnyStatisticState
+}
+
+type AnyStatisticState = interface{}
+
+type StartStopState struct {
+	Start int
+	Stop  int
+}
+
+type RoundState struct {
+	StartedRounds  int
+	FinishedRounds int
+}
+
+type PlayerContainerState struct {
+	Player map[string]*PlayerModel
+}
+
+type PlayerModel struct {
+	PlayerKey string
+	Uid       string
+	Connected bool
+	Name      string
+	Statistic map[string]int
+}
+
+type TeamContainerState struct {
+	Team map[string]int
+}
+
+type TeamModel struct {
+	TeamKey   string
+	Name      string
+	Statistic map[string]int
+	Player    map[string]bool
+}
