679bb94ed6cefb6de60c791d9552365372359411
Elmer Bulthuis <elmerbulthuis@gmail.com>
2018-08-23 11:44:05 +0200

    fixes #14, fixes #4

--- clients/gameye-config.go
+++ clients/gameye-config.go
@@ -1,5 +1,11 @@
 package clients
 
+import (
+	"errors"
+	"os"
+	"strings"
+)
+
 /*
 GameyeClientConfig configures the GameyeClient
 */
@@ -8,6 +14,30 @@ type GameyeClientConfig struct {
 	Token    string
 }
 
-func (config GameyeClientConfig) validate() {
-	// TODO
+/*
+ErrMissingConfigField occurs when a config field is missing
+*/
+var ErrMissingConfigField = errors.New("missing field in config")
+
+func (config *GameyeClientConfig) validate() (
+	err error,
+) {
+	if strings.TrimSpace(config.Endpoint) == "" {
+		config.Endpoint = os.Getenv("GAMEYE_API_ENDPOINT")
+	}
+
+	if strings.TrimSpace(config.Endpoint) == "" {
+		err = ErrMissingConfigField
+		return
+	}
+
+	if strings.TrimSpace(config.Token) == "" {
+		config.Token = os.Getenv("GAMEYE_API_TOKEN")
+	}
+	if strings.TrimSpace(config.Token) == "" {
+		err = ErrMissingConfigField
+		return
+	}
+
+	return
 }
