4604797eefdf64382157ca8da61c547eef989990
Elmer Bulthuis <elmer@gameye.com>
2018-08-23 12:48:05 +0200

    fixing state bug #15 (#17)
    fixed #15

--- utils/deep_test.go
+++ utils/deep_test.go
@@ -7,16 +7,33 @@ import (
 )
 
 func TestSetIn(t *testing.T) {
-	state := map[string]interface{}{}
-	value := "yeah"
+	initialState := map[string]interface{}{}
 
-	actual := SetIn(state, []string{"1", "2"}, value)
-	assert.Equal(t, map[string]interface{}{}, state)
+	actual := SetIn(initialState, []string{"1", "2"}, "yeah")
+	assert.Equal(t, map[string]interface{}{}, initialState)
 
 	expected := map[string]interface{}{
 		"1": map[string]interface{}{
-			"2": value,
+			"2": "yeah",
 		},
 	}
 	assert.Equal(t, expected, actual)
+
+	actual = SetIn(actual, []string{"1", "2"}, "yo")
+	expected = map[string]interface{}{
+		"1": map[string]interface{}{
+			"2": "yo",
+		},
+	}
+	assert.Equal(t, expected, actual)
+
+	actual = SetIn(actual, []string{"3"}, "ya")
+	expected = map[string]interface{}{
+		"1": map[string]interface{}{
+			"2": "yo",
+		},
+		"3": "ya",
+	}
+	assert.Equal(t, expected, actual)
+
 }
