d31893e368cae01213d62b4eff966716ae96e894
David Goemans <dgoemans@gmail.com>
2019-09-24 15:32:19 +0200

    More lint things

--- test/testutils/content.go
+++ test/testutils/content.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 )
 
+// ReadFileAsJSON reads a file and converts it to a json structure
 func ReadFileAsJSON(path string) (deserializedFile map[string]interface{}, err error) {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
